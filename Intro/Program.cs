36c70fa62ba228cd62415d790b0439e14dd4d37f
Kristof <kristof@aut.bme.hu>
2017-06-09 09:24:11 +0200

    Initial commit

---
+++ Intro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntroHelper helper = new IntroHelper();
+            Mat image = helper.CreateEmptyGreenImage();
+            var color = helper.GetPixelColor(image, new Point(10, 10));
+            Console.WriteLine("E");
+        }
+    }
+}
