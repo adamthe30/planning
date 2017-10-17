f64525e5eb51596f308426f98dcacb2aaac9907d
Kristof <kristof@aut.bme.hu>
2017-10-17 16:57:58 +0200

    BlobSizeHistogramTask solved, tests fixed

--- Intro/Solutions/BlobSizeHistogramTask.cs
+++ Intro/Solutions/BlobSizeHistogramTask.cs
@@ -1,16 +1,44 @@
-﻿using System;
+﻿using OpenCvSharp;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Intro.Solutions
 {
     public class BlobSizeHistogramTask
     {
-        public int GetBlobNumberInAreaRange(int minArea, int maxArea)
+        public int GetBlobNumberInAreaRange(Mat image, int minArea, int maxArea)
         {
-            return -1;
+            var areas = CollectBlobSizesGetBlobNumberInAreaRange(image);
+            return areas.FindAll(a => (a >= minArea && a <= maxArea)).Count;
+        }
+
+        public List<int> CollectBlobSizesGetBlobNumberInAreaRange(Mat image)
+        {
+            // Assumes CV_8UC1 images, 0 background, 255 blob
+            var result = new List<int>();
+            Mat img = image.Clone();
+            var indexer = img.GetGenericIndexer<byte>();
+            byte backgoundColor = 0;
+            byte tempColor = 1;
+            for (int i = 0; i < img.Cols; i++)
+                for (int j = 0; j < img.Rows; j++)
+                {
+                    byte c = indexer[j, i];
+                    if (c != backgoundColor)
+                    {
+                        Rect rect = new Rect();
+                        Cv2.FloodFill(img, new Point(i, j), tempColor, out rect, null, null, FloodFillFlags.Link4);
+                        int area = 0;
+                        for (int y = rect.Top; y <= rect.Bottom; y++)
+                            for (int x = rect.Left; x <= rect.Right; x++)
+                                if (indexer[y,x]== tempColor)
+                            {
+                                    area++;
+                                    indexer[y, x] = backgoundColor;
+                            }
+                        result.Add(area);
+                    }
+                }
+            return result;
         }
     }
 }
